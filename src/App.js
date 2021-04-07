2a8b94d890cae9bffb877f84492c14c5ab77310f
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-04-07 21:57:36 +0530

    added job component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
 import useFetchJobs from './useFetchJobs';
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
   const { jobs, loading, error } = useFetchJobs();
 
   return (
-    <Container>
+    <Container className='my-4'>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing </h1>}
-      {<h1>{jobs.length}</h1>}
+
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
     </Container>
   );
 }
